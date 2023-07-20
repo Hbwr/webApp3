d743194adc2e4f7e9338ac7c115af6d60930a3d4
Paul Norbert Hubweber <paul-norbert.hubweber@informatik.hs-fulda.de>
2023-07-20 11:38:28 +0200

    understood_httpUrl

--- README.md
+++ README.md
@@ -2,5 +2,8 @@
 
 ## HTTP
 
+locally (VS Code or Codespace Desktop)
+- http://127.0.0.1:3000/
+in Codespace
 - https://hbwr-ominous-dollop-xprjxgw5vqv36xx9-3000.preview.app.github.dev/
 - port is 3000
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,16 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+    res.render("index", { title: "Express" });
 });
 
 /* GET anything else. */
-router.get('/hello', function(req, res, next) {
-  res.status(200).send("Hello world");
+router.get("/hello", function (req, res, next) {
+    var fullUrl = req.protocol + "://" + req.get("host") + req.originalUrl;
+    console.log(fullUrl);
+    res.status(200).send("Hello world");
 });
 
 module.exports = router;
